504e55ce7c141158b19ec17e7313b9ba8d5ac13f
Jizizy <102010834+Jizizy@users.noreply.github.com>
2026-08-23 12:42:13 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 const whatsappUrl =
-  "https://wa.me/4915901316377?text=Hallo%2C%20ich%20m%C3%B6chte%20gerne%20einen%20Termin%20bei%20Olessia%20Epp%20vereinbaren.";
+  "https://wa.me/4915901316377?text=Hallo%2C%20ich%20m%C3%B6chte%20gerne%20einen%20Termin%20vereinbaren.";
 
 export default function Home() {
   return (
